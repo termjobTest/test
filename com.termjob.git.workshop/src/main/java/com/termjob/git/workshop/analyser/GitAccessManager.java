299e7b43649def90f23c48d2ab5dacd9d12cca36
Anoop Abraham <anoop@indiemakers.in>
2020-03-28 14:09:43 +0530

    commit git util class

---
+++ com.termjob.git.workshop/src/main/java/com/termjob/git/workshop/analyser/GitAccessManager.java
@@ -0,0 +1,40 @@
+package com.termjob.git.workshop.analyser;
+
+import java.io.File;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+/**
+ * @author Anoop
+ *
+ */
+public class GitAccessManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InvalidRemoteException, TransportException, GitAPIException {
+		Git.cloneRepository().setURI("https://github.com/eclipse/jgit.git")
+				.setDirectory(new File("/path/to/targetdirectory")) // #1
+				.call();
+		
+		
+		Git git = Git.cloneRepository()
+                .setURI("https://github.com/eclipse/jgit.git")
+                .setDirectory(new File("/path/to/targetdirectory"))
+//                .setTransportConfigCallback(getTransportConfigCallback())
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider("", ""))
+                .call();
+		Repository repo = git.getRepository();
+		
+		
+//		JGitUtil
+		
+	}
+
+}
