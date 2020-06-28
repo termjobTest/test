002d04e552ead83d4edfe740b989cc2365b5112a
Anoop Abraham <anoop@indiemakers.in>
2020-06-28 15:28:07 +0530

    commit java to sprint branch

---
+++ com.termjob.git.workshop/src/main/java/com/termjob/git/workshop/analyser/DumpJva.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.termjob.git.workshop.analyser;
+
+import java.util.List;
+
+/**
+ * @author Anoop
+ *
+ */
+public class DumpJva {
+
+	/**
+	 * 
+	 */
+	public DumpJva() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+//	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}	public void setFoo(List foo) {
+//		foo.add("anu");
+////		this.foo = foo; // compilation error, bcz compiler knows method can be call multiple time.
+//	}
+}
